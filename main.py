45e1f544f5be9f5d7c29de7567156c5a622581d7
Pawel Manka <pawelmanka@student.agh.edu.pl>
2021-04-28 17:09:29 +0200

    wydmuszka xD

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
+#funkcja redukująca macierz
+#posiada argument A: macierz do zredykowania
+
+def reduce_mat(A):
+    pass
+
+def find_indep_zeros(A):
+    pass
+
+def cross_out_minimal_line(A):
+    pass
+
 if __name__ == "__main__":
-    print(3)
+
