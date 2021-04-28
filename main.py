c68f3d2eeeca067aa82b324a03612ff7e10b29f4
Bartosz Żak <bartosz.zak0@gmail.com>
2021-04-28 18:03:01 +0200

    poprawka do wykreslania

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from copy import deepcopy
 from math import inf
 
-#w zadaniu będziemy iterować od zera! tzn indeks 0 oznacza pierwszy element
+# w zadaniu będziemy iterować od zera! tzn indeks 0 oznacza pierwszy element
 
-#funkcja redukująca macierz
+# funkcja redukująca macierz
 
-#posiada argument A: macierz do zredukowania
-from typing import List, Tuple
+# posiada argument A: macierz do zredukowania
+from typing import List, Tuple, Union
+
 INF = float('inf')
+
 
 # zwraca listę minimalnych wartości w kolumnach
 def find_min_in_col(A: List[List[int]]) -> List[int]:
@@ -21,6 +23,7 @@ def find_min_in_col(A: List[List[int]]) -> List[int]:
         minimal_val = INF
     return result_vector
 
+
 # zwraca listę minimalnych wartości w wierszach
 def find_min_in_row(A: List[List[int]]) -> List[int]:
     minimal_val = INF
@@ -33,11 +36,12 @@ def find_min_in_row(A: List[List[int]]) -> List[int]:
         minimal_val = INF
     return result_vector
 
-def reduce_mat(A):
 
+def reduce_mat(A):
     pass
 
-def cross_out_minimal_line(A: List[List]):
+
+def cross_out_minimal_line(A: List[List[Union[float, int]]]):
     zeros_rows = [0 for _ in range(len(A))]
     zeros_cols = [0 for _ in range(len(A))]
     A_copy = deepcopy(A)
@@ -54,19 +58,20 @@ def cross_out_minimal_line(A: List[List]):
         max_cols = max(zeros_cols)
         if max_rows >= max_cols:
             max_idx = zeros_rows.index(max(zeros_rows))
+            zeros_rows[max_idx] = 0
             crossed_r.append(max_idx)
             for col in range(len(A_copy)):
                 if A[max_idx][col] == 0:
-                    zeros_cols -= 1
-                A[max_idx][col] = inf
+                    zeros_cols[col] -= 1
+                A_copy[max_idx][col] = inf
         else:
             max_idx = zeros_cols.index(max(zeros_cols))
+            zeros_cols[max_idx] = 0
             crossed_c.append(max_idx)
             for row in range(len(A_copy)):
                 if A[row][max_idx] == 0:
-                    zeros_rows -= 1
-                A[row][max_idx] = inf
-
+                    zeros_rows[row] -= 1
+                A_copy[row][max_idx] = inf
     min_val = min((min(v) for v in A))
     for row in range(len(A_copy)):
         for col in range(len(A_copy)):
@@ -77,8 +82,8 @@ def cross_out_minimal_line(A: List[List]):
         for cc in crossed_c:
             A[cr][cc] += min_val
 
-def find_indep_zeros(A: List[List[int]]) -> List[Tuple[int]]:
 
+def find_indep_zeros(A: List[List[int]]) -> List[Tuple[int]]:
     lst_of_indep_zeros_tuple = []
     lst_of_taken_rows = []
     lst_of_taken_cols = []
@@ -92,7 +97,6 @@ def find_indep_zeros(A: List[List[int]]) -> List[Tuple[int]]:
     return lst_of_indep_zeros_tuple
 
 
-
 if __name__ == "__main__":
     A = [[1, 2, -1],
          [3, 4, 0],
@@ -100,8 +104,6 @@ if __name__ == "__main__":
 
     print(find_min_in_row(A))
 
-
     # A = [[5, 30, 0, 30], [65, 50, 0, 0], [0, 0, 55, 5], [55, 20, 0, 5]]
     print(find_indep_zeros(A))
-    print(find_min_in_row(A))
 
