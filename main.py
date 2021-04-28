a7127d52a0c6df2f26d6fa57b73770d3dc49cd3c
Kacper Kozaczko <kozaczko@student.agh.edu.pl>
2021-04-28 17:21:06 +0200

    uzupełniona funkcja znajdująca minimalną wartość w wierszach

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 #funkcja redukująca macierz
 #posiada argument A: macierz do zredukowania
 from typing import List
+INF = float('inf')
 
 # zwraca listę minimalnych wartości w kolumnach
-def find_min_in_vec(A: List[List[int]]) -> List[int]:
+def find_min_in_col(A: List[List[int]]) -> List[int]:
     pass
 
 # zwraca listę minimalnych wartości w wierszach
 def find_min_in_row(A: List[List[int]]) -> List[int]:
-    pass
+    minimal_val = INF
+    result_vector = []
+    for i in range(len(A)):
+        for j in range(len(A)):
+            if A[i][j] < minimal_val:
+                minimal_val = A[i][j]
+        result_vector.append(minimal_val)
+        minimal_val = INF
+    return result_vector
 
 def reduce_mat(A):
     pass
@@ -19,5 +28,9 @@ def find_indep_zeros(A):
 def cross_out_minimal_line(A):
     pass
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
+    A = [[1, 2, -1],
+         [3, 4, 0],
+         [0, 1, 3]]
+    print(find_min_in_row(A))
